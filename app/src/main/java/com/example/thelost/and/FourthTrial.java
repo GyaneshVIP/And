c37767180214052210fd91f658acfbb81ca990c0
StarkAndy <andystark.developer@gmail.com>
2017-10-11 01:14:15 -1200

    Second Intial Commit

---
+++ app/src/main/java/com/example/thelost/and/FourthTrial.java
@@ -0,0 +1,13 @@
+package com.example.thelost.and;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class FourthTrial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth_trial);
+    }
+}
